aabdc6be68bb81df189b98fce9ffd6da8a88753f
redaelemawy <redaelemawy98@gmail.com>
2025-04-10 19:40:41 +0200

    feat: explore tsConfig=> noUncheckedIndexedAccess

--- src/string-key-of.ts
+++ src/string-key-of.ts
@@ -10,29 +10,39 @@
  *
  * The function is then called with different type parameters to demonstrate that the correct sub-set of Action is inferred.
  *
-* */
+ * */
 
-type Action = {
-    type: "LOG_IN",
-    payload: {
-        email: string,
-        password: string
+type Action =
+  | {
+      type: 'LOG_IN';
+      payload: {
+        email: string;
+        password: string;
+      };
     }
-} | {
-    type: "LOG_OUT"
-} | {
-    type: "REGISTER",
-    payload: {
-        email: string,
-        password: string,
-        name: string,
-        age: number
+  | {
+      type: 'LOG_OUT';
     }
-};
+  | {
+      type: 'REGISTER';
+      payload: {
+        email: string;
+        password: string;
+        name: string;
+        age: number;
+      };
+    };
 
-function sendEvent<Type extends Action["type"]>(...args:Extract<Action, {type: Type}> extends {payload: infer Payload} ? [Type, Payload] : [Type]) {
-}
+function sendEvent<Type extends Action['type']>(
+  ...args: Extract<Action, { type: Type }> extends { payload: infer Payload }
+    ? [Type, Payload]
+    : [Type]
+) {}
 
-sendEvent("LOG_IN", {email: "", password: ""});
-sendEvent("LOG_OUT");
-sendEvent("REGISTER", {email: "", password: "", name: "", age: 0});
+sendEvent('LOG_IN', { email: '', password: '' });
+sendEvent('LOG_OUT');
+sendEvent('REGISTER', { email: '', password: '', name: '', age: 0 });
+
+const myObj: Record<string, string> = {};
+// tsConfig => noUncheckedIndexedAccess = false
+myObj.foo.length; // works but could lead to bugs if foo is not in the object
